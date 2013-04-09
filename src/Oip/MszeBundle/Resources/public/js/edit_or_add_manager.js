14f31431a2739f6628245a31dde54431c4c8d5a9
Piotr Wyrobek <pwyrobek@proximetry.pl>
2013-04-09 19:47:40 +0200

    more thing done

--- src/Oip/MszeBundle/Resources/public/js/edit_or_add_manager.js
+++ src/Oip/MszeBundle/Resources/public/js/edit_or_add_manager.js
@@ -37,51 +37,31 @@ $.oip.managerDef = function(city_id, district_id, church_id, fn) {
         fun_show_save = fn["fun_show_save"],
 
         self = this,
-        _city_id = city_id, _city_new_name, _cur_city_name,
+        _city = {},
+        _district = {},
+        _church = { },
+        _mass = { 
+            del_list: {},
+            mod_list: {}
+        },
         
-        _district_id = district_id, _district_new_name, _cur_district_name,
-        
-        _church_id = church_id, _church_new_name,
-        _cur_church_address, _cur_church_desc = '',
-        _pos_lat, _pos_lng,
-        
-        _new_masses_list = {},
-        _del_masses_list = {},
-        _mod_masses_list = {},
-        _smasses,
-        _wmasses,
         _new_mass_id_cnt = -1,
-    
         _address_setup_allow = false,
         
         _local_clear_city = function () { 
-            _city_id = -1;
-            
-            _city_new_name = undefined;
-            _cur_city_name = undefined;
+            _city = { id: -1 };
         },
         
         _local_clear_district = function () {
-            _district_id = -1;
-            
-            _district_new_name = undefined;
-            _cur_district_name = undefined;
+            _district = { id: -1 };
         },
         
         _local_clear_church = function () {
-            _church_id = -1;
-            
-            _church_new_name = undefined;
-            _cur_church_address = undefined;
-            _cur_church_desc = undefined;
-            _pos_lat = undefined;
-            _pos_lng = undefined;
-            
-            _new_masses_list = {};
-            _del_masses_list = {};
-            _mod_masses_list = {};
-            _smasses = undefined;
-            _wmasses = undefined;
+            _church = { id: -1 }
+            _mass = { 
+                del_list: {},
+                mod_list: {}
+            };
         },
         
         _local_extract_time_and_id = function(p_masses) {
@@ -113,25 +93,60 @@ $.oip.managerDef = function(city_id, district_id, church_id, fn) {
             ret.sort(lcompare);
             
             return ret;
+        },
+        
+        _local_show_hide_save = function() {
+            if (
+                    (_city.id < 0 && _city.new_name != undefined) || //new city
+                    (_city.id >= 0 && _city.cur_name != _city.new_name) || //name change
+                
+                
+                    //addd district change
+                    (_district.id < 0 && _district.new_name != undefined) || //new district
+                    (_district.id >= 0 && _district.cur_name != _district.cur_name) || //name change
+                        
+                    (_church.id < 0 && _church.new_name != undefined) || // new church
+                    (_church.id >= 0 && _church.cur_name != _church.new_name) || //name change
+                        
+                    (_church.id >= 0 && _church.cur_address != _church.new_address) || //address change
+                    (_church.id >= 0 && _church.cur_desc != _church.new_desc) || //desc change
+                    
+                    (_church.id >= 0 && _mass.del_list.length > 0) || //something to delete
+                    (_church.id >= 0 && _mass.mod_list.length > 0) || //something to mod or add
+                    false
+                )
+            {
+                fun_show_save();
+            }
+            else
+            {
+                fun_hide_save();
+            }
         }
         ;
     
     self.setChurchAddress = function(address) {
-        _cur_church_address = address;
+        _church["new_address"] = address;
+        //_new_church_address = address;
     }
     self.setChurchDescription = function(desc) {
-        _cur_church_desc = desc;
+        _church["new_desc"] = desc;
+        //_new_church_desc = desc;
     }
     self.setupPosition = function(lat, lng) {
-        _pos_lat = lat;
-        _pos_lng = lng;
+        _church["new_pos"] = {};
+        _church.new_pos["lat"] = lat;
+        _church.new_pos["lng"] = lng;
+    }
+    self.resetPosition = function() {
+        _church["new_pos"] = {};
     }
     self.getGoogleGPSPos = function() {
-        if (_pos_lat == undefined || _pos_lng == undefined)
+        if (_church.new_pos == undefined)
         {
             return undefined;
         }
-        return { lat: _pos_lat, lng: _pos_lng };
+        return { lat: _church.new_pos.lat, lng: _church.new_pos.lng };
     }
     
     self.getNewMassId = function()
@@ -140,35 +155,32 @@ $.oip.managerDef = function(city_id, district_id, church_id, fn) {
         return _new_mass_id_cnt;
     }
     
-    self.getCityId = function() { return _city_id; }
-    self.getChurchId = function() { return _church_id; }
-    self.getDistrictId = function() { return _district_id; }
+    self.getCityId = function() { return _city.id; }
+    self.getChurchId = function() { return _church.id; }
+    self.getDistrictId = function() { return _district.id; }
     self.getGoogleSearch = function() 
     {      
         var _ret = '';
-        if (_cur_city_name != undefined && _cur_city_name != '') {
-            _ret += _cur_city_name;
+        if (_city.new_name != undefined && _city.new_name != '') {
+            _ret += _city.new_name;
         }
-        /*if (_cur_district_name != undefined && _cur_district_name != '') {
-            _ret += ', ' + _cur_district_name;
-        }*/
-        if (_cur_church_address != undefined && _cur_church_address != '') {
-            _ret += ', ' + _cur_church_address;
+        if (_church.new_address != undefined && _church.new_address != '') {
+            _ret += ', ' + _church.new_address;
         }
         return _ret == '' ? undefined : _ret;
     }
     
     self.renderMasses = function() {
-        var n_smasses = $.extend({}, _smasses),
-            n_wmasses = $.extend({}, _wmasses);
+        var n_smasses = $.extend({}, _mass.smasses),
+            n_wmasses = $.extend({}, _mass.wmasses);
         
         for (var ind in n_smasses) {
             n_smasses[ind]['del'] = false;
             n_smasses[ind]['mod'] = false;
-           if (_del_masses_list[ind] != undefined) {
+           if (_mass.del_list[ind] != undefined) {
                n_smasses[ind]['del'] = true;
-           } else if (_mod_masses_list[ind] != undefined) {
-               n_smasses[ind] = _mod_masses_list[ind];
+           } else if (_mass.mod_list[ind] != undefined) {
+               n_smasses[ind] = _mass.mod_list[ind];
                n_smasses[ind]['mod'] = true;
            }
         }
@@ -176,23 +188,22 @@ $.oip.managerDef = function(city_id, district_id, church_id, fn) {
         for (ind in n_wmasses) {
             n_wmasses[ind]['del'] = false;
             n_wmasses[ind]['mod'] = false;
-            if (_del_masses_list[ind] != undefined) {
+            if (_mass.del_list[ind] != undefined) {
                n_wmasses[ind]['del'] = true;
-           } else if (_mod_masses_list[ind] != undefined) {
-               n_wmasses[ind] = _mod_masses_list[ind];
+           } else if (_mass.mod_list[ind] != undefined) {
+               n_wmasses[ind] = _mass.mod_list[ind];
                n_wmasses[ind]['mod'] = true;
            }
         }
         
-        for (ind in _mod_masses_list)
+        for (ind in _mass.mod_list)
         {
-           if (ind < 0 && _mod_masses_list[ind]['day_sun'] == true) {
-               n_smasses[ind] = _mod_masses_list[ind];
-           } else if (ind < 0 && _mod_masses_list[ind]['day_sun'] == false) {
-               n_wmasses[ind] = _mod_masses_list[ind];
+           if (ind < 0 && _mass.mod_list[ind]['day_sun'] == true) {
+               n_smasses[ind] = _mass.mod_list[ind];
+           } else if (ind < 0 && _mass.mod_list[ind]['day_sun'] == false) {
+               n_wmasses[ind] = _mass.mod_list[ind];
            } 
         }
-        
         
         fun_mass_hide();
         fun_mass_fill(_local_make_array(n_smasses), _local_make_array(n_wmasses))
@@ -203,12 +214,12 @@ $.oip.managerDef = function(city_id, district_id, church_id, fn) {
         _local_clear_city()
         _local_clear_district();
         _local_clear_church();
-        _city_id = id;
+        _city.id = id;
             
         if (id == -1 && isNew == true)
         {
-            _city_new_name = newName;
-            _cur_city_name = newName;
+            _city.new_name = newName;
+            _city.cur_name = newName;
             
             fun_city_hide_edit();
             fun_district_hide_edit();
@@ -225,13 +236,12 @@ $.oip.managerDef = function(city_id, district_id, church_id, fn) {
             
             fun_mass_hide();
             fun_mass_clear();
-            
             if (afterFun != undefined) { afterFun(); }
             
-            fun_show_save();
+            _local_show_hide_save();
         }
         else if (id == -1 && isNew !== true)
-        {
+        {            
             fun_city_hide_edit();
             fun_district_hide_edit();
             fun_church_hide_edit();
@@ -244,16 +254,23 @@ $.oip.managerDef = function(city_id, district_id, church_id, fn) {
             fun_district_hide();
             fun_city_unselect();
             if (afterFun != undefined) { afterFun(); }
+            
+            _local_show_hide_save();
         }
         else if (id != -1)
         {
             $.oip.ajax.getJSON(('fast_city'), { id: id }, null, function(data) {
-                _cur_city_name = data.name;
+                _city.cur_name = data.name;
+                _city.new_name = data.name;
+                
                 fun_city_select(data.name);
                 fun_district_fill(data.districts)
                 fun_district_show();
+                
                 if (afterFun != undefined) { afterFun(); }
                 fun_city_show_edit();
+                
+                _local_show_hide_save();
             });
         }
     }
@@ -261,17 +278,12 @@ $.oip.managerDef = function(city_id, district_id, church_id, fn) {
     self.setupDistrictId = function(id, isNew, newName, afterFun) {
         _local_clear_district();
         _local_clear_church();
-        _district_id = id;
-        if (id == -100) {
-            id = -1;
-            newName = '';
-            isNew = true;
-        }
+        _district.id = id;
         
         if (id == -1 && isNew == true)
         {
-            _district_new_name = newName;
-            _cur_district_name = newName;
+            _district.new_name = newName;
+            _district.cur_name = newName;
             
             fun_district_hide_edit();
             fun_church_hide_edit();
@@ -282,7 +294,8 @@ $.oip.managerDef = function(city_id, district_id, church_id, fn) {
             fun_mass_hide();
             fun_mass_clear();
             if (afterFun != undefined) { afterFun(); }
-            fun_show_save();
+            
+            _local_show_hide_save();
         }
         else if (id == -1 && isNew !== true)
         {
@@ -295,29 +308,38 @@ $.oip.managerDef = function(city_id, district_id, church_id, fn) {
             fun_church_hide();
             fun_district_unselect();
             if (afterFun != undefined) { afterFun(); }
+            
+            _local_show_hide_save();
         }
         else if (id != -1)
         {
             $.oip.ajax.getJSON(('fast_district'), { id: id }, null, function(data) {
-                _cur_district_name = data.name;
+                _district.cur_name = data.name;
+                _district.new_name = data.name;
+                
                 fun_district_select(data.name);
                 fun_church_fill(data.churches)
                 fun_church_show();
+                
                 if (afterFun != undefined) { afterFun(); }
                 if (data.name != '')
                 {
                     fun_district_show_edit();
                 }
+                
+                _local_show_hide_save();
             });
         }
     }
     
     self.setupChurchId = function(id, isNew, newName, afterFun) {
         _local_clear_church();
-        _church_id = id;
+        _church.id = id;
+        
         if (id == -1 && isNew == true)
         {
-            _church_new_name = newName;
+            _church.cur_name = newName;
+            _church.new_name = newName;
             
             _address_setup_allow = true;
             fun_church_hide_edit();
@@ -325,10 +347,11 @@ $.oip.managerDef = function(city_id, district_id, church_id, fn) {
             fun_mass_clear();
             fun_mass_show();
             if (afterFun != undefined) { afterFun(); }
-            fun_show_save();
             
             fun_mass_fill([],[]);
             fun_mass_show();
+            
+            _local_show_hide_save();
         }
         else if (id == -1 && isNew !== true)
         {
@@ -339,56 +362,67 @@ $.oip.managerDef = function(city_id, district_id, church_id, fn) {
             fun_mass_hide();
             fun_mass_clear();
             if (afterFun != undefined) { afterFun(); }
+            
+            _local_show_hide_save();
         }
         else if (id != -1)
         {
             $.oip.ajax.getJSON(('fast_church'), { id: id }, null, function(data) {
-                _cur_church_address = data.address;
-                _cur_church_desc = data.description;
-                _pos_lat = data.latitude;
-                _pos_lng = data.longitude;
+                _church.cur_address = data.address;
+                _church.new_address = data.address;
+                
+                _church.cur_desc = data.description;
+                _church.new_desc = data.description;
+                
+                _church.cur_pos = {};
+                _church.cur_pos.lat = data.latitude;
+                _church.cur_pos.lng = data.longitude;
+                self.setupPosition(data.latitude, data.longitude);
+                
                 _address_setup_allow = true;
                 fun_church_select(data.name, data);
-                _smasses = _local_extract_time_and_id(data.smasses);
-                _wmasses = _local_extract_time_and_id(data.wmasses);
-                fun_mass_fill(_local_make_array(_smasses), _local_make_array(_wmasses))
+                _mass.smasses = _local_extract_time_and_id(data.smasses);
+                _mass.wmasses = _local_extract_time_and_id(data.wmasses);
+                fun_mass_fill(_local_make_array(_mass.smasses), _local_make_array(_mass.wmasses))
                 fun_mass_show();
                 if (afterFun != undefined) { afterFun(); }
                 fun_church_show_edit();
+                
+                _local_show_hide_save();
             });
         }
     } 
     
     self.isMarkedForDeletion = function(id) {
-        return _del_masses_list[id] != undefined;
+        return _mass.del_list[id] != undefined;
     }
     
     self.markMassForDeletion = function(id) {
-        debugger;
         if (id < 0)
         {
-            delete _mod_masses_list[id];
+            delete _mass.mod_list[id];
         }
         else
         {
-           _del_masses_list[id] = true; 
+           _mass.del_list[id] = true;
+           fun_show_save();
         }
         self.renderMasses();
     }
     
     self.unmarkMassForDeletion = function(id) {
-        delete _del_masses_list[id];
+        delete _mass.del_list[id];
+        
         self.renderMasses();
     }
     
     self.markMassForChange = function(id, values) {
-        debugger;
-        _mod_masses_list[id] = values;
+        _mass.mod_list[id] = values;
         self.renderMasses();
     }
     
     self.unmarkMassForChange = function(id) {
-        delete _mod_masses_list[id];
+        delete _mass.mod_list[id];
         self.renderMasses();
     }
     
@@ -397,15 +431,15 @@ $.oip.managerDef = function(city_id, district_id, church_id, fn) {
     }
     
     self.Save = function(re_c, re_r) {
-        var str = { 'cid': _city_id, 'cname': _city_new_name, 
-                'did': _district_id, 'dname': _district_new_name,
-                'chid': _church_id, 'chname': _church_new_name,
-                'caddr': _cur_church_address,
-                'cdesc': _cur_church_desc,
-                'clat': _pos_lat,
-                'clng': _pos_lng,
-                'mmod': _mod_masses_list,
-                'mdel': _del_masses_list
+        var str = { 'cid': _city.id, 'cname': _city.new_name, 
+                'did': _district.id, 'dname': _district.new_name,
+                'chid': _church.id, 'chname': _church.new_name,
+                'caddr': _church.new_address,
+                'cdesc': _church.new_desc,
+                'clat': _church.new_pos.lat,
+                'clng': _church.new_pos.lng,
+                'mmod': _mass.mod_list,
+                'mdel': _mass.del_list
               };
         $.oip.ajax.postJSON('save_all', null, {'all_data': str, 're_c': re_c, 're_r': re_r}, function(data) {
             if (data['error'] != undefined) {
