6ae7c243ebd86a1bc9d130bfb59d29a9691fb4b3
Piotr Wyrobek <pwyrobek@proximetry.pl>
2013-04-17 18:03:05 +0200

    web data

---
+++ web/bundles/oipmsze/js/mszeajax.js
@@ -0,0 +1,53 @@
+
+$.oip = $.oip || {}
+$.oip.ajax = $.oip.ajax || {}
+
+$.oip.ajax.getCities = function(pattern, callback) {
+    
+    if (pattern != '')
+    {
+        $.oip.ajax.get(('show_cities'), { _format: 'json' }, 
+            { s: pattern }, callback);
+    }
+}
+
+$.oip.ajax.getJSON = function(route, route_params, data, callback) {
+    $.get(
+        Routing.generate(route, route_params), 
+        data, 
+        function(data) 
+        {                
+            callback(data); 
+        }, 'json');
+}
+
+$.oip.ajax.get = function(route, route_params, data, callback) {
+    $.get(
+        Routing.generate(route, route_params), 
+        data, 
+        function(data) 
+        {                
+            callback(data); 
+        });
+}
+
+$.oip.ajax.postJSON = function(route, route_params, data, callback) {
+    $.post(
+        Routing.generate(route, route_params), 
+        data, 
+        function(data) 
+        {                
+            callback(data); 
+        },
+        'json');
+}
+
+$.oip.ajax.post = function(route, route_params, data, callback) {
+    $.post(
+        Routing.generate(route, route_params), 
+        data, 
+        function(data) 
+        {                
+            callback(data); 
+        });
+}
