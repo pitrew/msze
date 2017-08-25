37717d1d911dc545f4177a2e9481200155f68377
Piotr Wyrobek <piotr.wyrobek@williamhill.com>
2017-08-25 20:02:27 +0200

    mysql

--- app/config/params.php
+++ app/config/params.php
@@ -1,9 +1,9 @@
 <?php
 # app/config/params.php
-if (getEnv("OPENSHIFT_APP_NAME")!='') {
-	$container->setParameter('database_host', getEnv("OPENSHIFT_MYSQL_DB_HOST"));
-	$container->setParameter('database_port', getEnv("OPENSHIFT_MYSQL_DB_PORT"));
-	$container->setParameter('database_name', getEnv("OPENSHIFT_APP_NAME"));
-	$container->setParameter('database_user', getEnv("OPENSHIFT_MYSQL_DB_USERNAME"));
-	$container->setParameter('database_password', getEnv("OPENSHIFT_MYSQL_DB_PASSWORD"));
+if (getEnv("MYSQL_USER")!='') {
+	$container->setParameter('database_host', getEnv("mysql_SERVICE_HOST"));
+	$container->setParameter('database_port', getEnv("mysqli_SERVICE_PORT"));
+	$container->setParameter('database_name', "msze");
+	$container->setParameter('database_user', getEnv("MYSQL_USER"));
+	$container->setParameter('database_password', getEnv("MYSQL_PASSWORD"));
 }?>
