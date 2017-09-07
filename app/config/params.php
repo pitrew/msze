44dad9a9aa23bb30b28898454d1fcb4863639207
Piotr Wyrobek <piotr.wyrobek@williamhill.com>
2017-09-07 11:45:21 +0200

    update params

--- app/config/params.php
+++ app/config/params.php
@@ -2,7 +2,7 @@
 # app/config/params.php
 if (getEnv("MYSQL_USER")!='') {
 	$container->setParameter('database_host', getEnv("mysql_SERVICE_HOST"));
-	$container->setParameter('database_port', getEnv("mysqli_SERVICE_PORT"));
+	$container->setParameter('database_port', getEnv("mysql_SERVICE_PORT"));
 	$container->setParameter('database_name', "msze");
 	$container->setParameter('database_user', getEnv("MYSQL_USER"));
 	$container->setParameter('database_password', getEnv("MYSQL_PASSWORD"));
