495902d1af8391ff2519675947f03545ef0465a3
Piotr Wyrobek <piotr.wyrobek@williamhill.com>
2017-09-13 17:22:22 +0200

    ehhh

--- app/config/params.php
+++ app/config/params.php
@@ -1,9 +1,15 @@
 <?php
 # app/config/params.php
-if (getEnv("MYSQL_USER")!='') {
-	$container->setParameter('database_host', getEnv("mysql_SERVICE_HOST"));
-	$container->setParameter('database_port', getEnv("mysql_SERVICE_PORT"));
+if (getEnv("MYSQL_SERVICE_HOST")!='') {
+	$container->setParameter('database_host', getEnv("MYSQL_SERVICE_HOST"));
+	$container->setParameter('database_port', getEnv("MYSQL_SERVICE_PORT"));
+	// $container->setParameter('database_name', getEnv("MYSQL_DATABASE"));
+	// $container->setParameter('database_user', getEnv("MYSQL_USER"));
+	// $container->setParameter('database_password', getEnv("MYSQL_PASSWORD"));
+
 	$container->setParameter('database_name', "msze");
-	$container->setParameter('database_user', getEnv("MYSQL_USER"));
-	$container->setParameter('database_password', getEnv("MYSQL_PASSWORD"));
+	$container->setParameter('database_user', "adminquM8Lrf");
+	$container->setParameter('database_password', "c3a9Xnuycipb");
 }?>
+}?>
+
