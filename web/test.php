274b1a2632d3018e4c2efbadc328a42de2cc5fa6
Piotr Wyrobek <piotr.wyrobek@casumo.com>
2019-01-02 10:57:28 +0100

    test2

--- web/test.php
+++ web/test.php
@@ -1,3 +1,29 @@
 <?php
+echo "Hello";
+echo getEnv("JAWSDB_URL");
 
-echo getEnv("DATABASE_URL");
+if (getEnv("JAWSDB_URL")!='') {
+	$url = getenv('JAWSDB_URL');
+	$dbparts = parse_url($url);
+
+	$hostname = $dbparts['host'];
+	$username = $dbparts['user'];
+	$password = $dbparts['pass'];
+	$port = $dbparts['port'];
+	$database = ltrim($dbparts['path'],'/');
+
+    echo "<br/>";
+    echo $hostname;
+    echo "<br/>";
+    echo $port;
+    echo "<br/>";
+	
+    echo $database;
+    echo "<br/>";
+    echo $username;
+    echo "<br/>";
+    // echo $password;
+    echo "<br/>";
+} else {
+    echo 'NIEEE';
+}
